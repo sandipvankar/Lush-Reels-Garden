c89b8ab425a22504dcbb3097736d74eb438ae049
sandipvankar <sandipvnkr1998@gmail.com>
2024-08-30 20:16:14 +0530

    Initial Commit

---
+++ Lush Reels Garden/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lush Reels Garden
+//
+//  Created by sandip  on 30/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lush Reels Garden/Lush_Reels_GardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lush_Reels_GardenApp.swift
+//  Lush Reels Garden
+//
+//  Created by sandip  on 30/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Lush_Reels_GardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
