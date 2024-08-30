956564d8c5654daa8c2e1e964123cc1dc2b2cec4
sandipvankar <sandipvnkr1998@gmail.com>
2024-08-30 20:41:14 +0530

    - create two screen with bgimage

---
+++ Lush Reels Garden/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  Lush Reels Garden
+//
+//  Created by sandip  on 30/08/24.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
